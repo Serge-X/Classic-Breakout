3542f221995363937ce20d6721fb0eb496ca4e06
Serge <lionheart.sergex@gmail.com>
2021-06-29 21:22:08 -0700

    Added draw function

--- gameLogic.js
+++ gameLogic.js
@@ -1,20 +1,29 @@
 let canvas = document.getElementById("gameSpace");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.arc(240,120,20,0, Math.PI*2, false);
-ctx.fillStyle = "#0000FF";
-ctx.fill();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.rect(160,10,100,40);
-ctx.strokeStyle = "rgba(0,0,255,0.5)";
-ctx.stroke();
-ctx.closePath();
+const DrawBall = () => 
+{
+    ctx.beginPath()
+    ctx.arc(x,y, 10,0, Math.PI*2);
+    ctx.fillStyle= "#0095DD"
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+const Draw= () =>
+{
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.beginPath();
+    DrawBall();
+    x+=dx
+    y+=dy
+}
+
+setInterval(Draw, 10)
