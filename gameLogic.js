0b24131e5dbcba7afab23139d6535a46266c1078
Serge <lionheart.sergex@gmail.com>
2021-06-30 18:47:14 -0700

    Added bounce logic to ball

--- gameLogic.js
+++ gameLogic.js
@@ -5,12 +5,15 @@ let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+
+
 
 
 const DrawBall = () => 
 {
     ctx.beginPath()
-    ctx.arc(x,y, 10,0, Math.PI*2);
+    ctx.arc(x,y, ballRadius,0, Math.PI*2);
     ctx.fillStyle= "#0095DD"
     ctx.fill();
     ctx.closePath();
@@ -22,8 +25,28 @@ const Draw= () =>
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.beginPath();
     DrawBall();
-    x+=dx
-    y+=dy
+
+    if(y + dy < ballRadius)
+    {
+        dy=-dy;
+    }
+
+    if(x + dx < ballRadius)
+    {
+        dx=-dx;
+    }
+
+    if(y + dy > canvas.height-ballRadius)
+    {
+        dy= -dy;
+    }
+    if(x + dx > canvas.width-ballRadius)
+    {
+        dx= -dx;
+    }
+
+        x+=dx
+        y+=dy
 }
 
 setInterval(Draw, 10)
