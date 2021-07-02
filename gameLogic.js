cff4b2e7eb959a6d414e2005a523771aebbf3f25
Serge <lionheart.sergex@gmail.com>
2021-07-02 14:15:28 -0700

    Added paddle and corrected function names

--- gameLogic.js
+++ gameLogic.js
@@ -1,16 +1,26 @@
 let canvas = document.getElementById("gameSpace");
 let ctx = canvas.getContext("2d");
 
+//Ball
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
 
+// Paddle
+let paddleHeight= 10;
+let paddleWidth= 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+
+//keyboard inputs
+let rightPressed = false;
+let leftPressed = false;
 
 
 
-const DrawBall = () => 
+
+const drawBall = () => 
 {
     ctx.beginPath()
     ctx.arc(x,y, ballRadius,0, Math.PI*2);
@@ -19,13 +29,46 @@ const DrawBall = () =>
     ctx.closePath();
 }
 
-
-const Draw= () =>
+const drawPaddle = () =>
 {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle ="#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+const keyDownHandler= (e) =>
+{
+    if (e.key == "Right" || e.key == "ArrowRight") 
+    {
+        rightPressed=true;
+
+    } else if(e.key == "Left" || e.key == "ArrowLeft")
+            {
+                leftPressed= true;
+            }
+}
+
+const keyUpHandler= (e) =>
+{
+    if (e.key == "Right" || e.key == "ArrowRight") 
+    {
+        rightPressed=false;
+
+    } else if(e.key == "Left" || e.key == "ArrowLeft")
+            {
+                leftPressed= false;
+            }
+}
+
+const draw= () =>
+{
+    
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.beginPath();
-    DrawBall();
-
+    drawBall();
+    drawPaddle();
     if(y + dy < ballRadius)
     {
         dy=-dy;
@@ -45,8 +88,29 @@ const Draw= () =>
         dx= -dx;
     }
 
-        x+=dx
-        y+=dy
+    // check if paddle is pressed
+    if (rightPressed) 
+    {
+        paddleX += 7;
+        if(paddleX + paddleWidth > canvas.width)
+            {
+                paddleX= canvas.width - paddleWidth;
+            }
+
+    } else if (leftPressed)
+    {
+        paddleX-= 7;
+        if(paddleX < 0)
+        {
+            paddleX =0;
+        }
+    }
+
+    x+=dx
+    y+=dy
 }
 
-setInterval(Draw, 10)
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+setInterval(draw, 10)
